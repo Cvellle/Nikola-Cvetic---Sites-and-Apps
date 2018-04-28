630b05534f7fb4ef6d537a33506cc58524fbc514
Cvele <cuekisa@yahoo.com>
2018-04-28 12:16:20 +0200

    a
    a

--- js/script.js
+++ js/script.js
@@ -2,14 +2,14 @@
 
 text1.onkeydown = function(e) {
     
-    if (e.keyCode > 57 || e.keyCode<48) {
+    if (e.keyCode > 57 || e.keyCode<48 && e.keyCode!=8) {
 
 		alert('Please input only numbers');	
 		return false;
 
 	}
 
-	if (e.keyCode<=57 && text1.value.length>0) {
+	if (e.keyCode<=57 && text1.value.length>0 && e.keyCode!=8) {
 
 		submit.disabled=false;
 		form1.text1.focus();
