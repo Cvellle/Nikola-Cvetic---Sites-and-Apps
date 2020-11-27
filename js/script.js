080a243e4c0deec4fbd55ef27772c6405fd371a4
cvellle <cuekisa@yahoo.com>
2020-11-27 12:35:57 +0100

    CV instructtions text fix

--- js/script.js
+++ js/script.js
@@ -1,27 +1,41 @@
-// FORM 
+// FORM
 text1.onkeydown = function (e) {
-	if (e.keyCode > 57 || e.keyCode < 48 && e.keyCode != 8) {
-		alert('Please input only numbers');
-		return false;
-	}
-	if (e.keyCode <= 57 && text1.value.length > 0 && e.keyCode != 8) {
-		submit.disabled = false;
-		form1.text1.focus();
-		return true;
-	}
-}
+  if (e.keyCode > 57 || (e.keyCode < 48 && e.keyCode != 8)) {
+    alert("Please input only numbers");
+    return false;
+  }
+  if (e.keyCode <= 57 && text1.value.length > 0 && e.keyCode != 8) {
+    submit.disabled = false;
+    form1.text1.focus();
+    return true;
+  }
+};
 
 //UNLOCK
 submit.addEventListener("click", subm);
 const wd = document.querySelector("#worksdiv");
 
 function subm() {
-	document.querySelector(".highlight").style.cursor = "pointer";
-	document.querySelector(".highlight").setAttribute("style", "pointer-events:fill");
-	document.querySelector(".highlight").style.webkitFilter = "brightness(1.2)";
-	document.querySelector(".form1").style.background = "blue";
-	instr.innerHTML = "Sucess! Now click on the Sattelite";
+  document.querySelector(".highlight").style.cursor = "pointer";
+  document
+    .querySelector(".highlight")
+    .setAttribute("style", "pointer-events:fill");
+  document.querySelector(".highlight").style.webkitFilter = "brightness(1.2)";
+  document.querySelector(".form1").style.background = "blue";
+  instr.innerHTML = "Sucess! Now click on the Sattelite";
 }
 
+//CLICK ON THE CV
+const cvLink = document.querySelector("#circlingLink");
+const introduction = document.querySelector(".introduction > p");
 
+cvLink.addEventListener("click", changeTextAfterCvClick);
 
+function changeTextAfterCvClick() {
+  instr.innerHTML = " ";
+  const unlockSpan =
+    introduction.firstChild.nextElementSibling.nextElementSibling
+      .nextElementSibling;
+  unlockSpan.style.display = "none";
+  unlockSpan.nextElementSibling.style.display = "inline";
+}
