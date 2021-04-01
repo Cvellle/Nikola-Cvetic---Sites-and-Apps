69bd82054da815c9379476603ca4fc9861fc2f13
cvellle <cuekisa@yahoo.com>
2021-04-01 14:28:32 +0200

    descriptions

--- js/showProjects.js
+++ js/showProjects.js
@@ -1,29 +1,26 @@
 function showProjects() {
+  const works = document.querySelector(".works-wrapper");
+  const hoverPreview = document.querySelectorAll(".work div");
 
-    const works = document.querySelector(".works-wrapper");
-    const hoverPreview = document.querySelectorAll(".work div");
+  const imgWrapper = document.querySelector(".imgDiv");
+  const imagePrew = document.querySelector(".preview");
+  const description = document.querySelector(".descrText");
 
-    const imagePrew = document.querySelector(".preview");
-    const imgWrapper = document.querySelector(".imgDiv");
+  hoverPreview.forEach((w) => w.addEventListener("mouseover", showProject));
+  works.addEventListener("mouseleave", hideProject);
 
-    hoverPreview.forEach(w => w.addEventListener("mouseover", showProject));
-    works.addEventListener("mouseleave", hideProject);
+  function showProject(e) {
+    imgWrapper.classList.remove("dn");
+    imgWrapper.classList.add("db");
+    imagePrew.classList.add("tr03");
+    imagePrew.classList.add("op1");
+    imagePrew.src = this.parentElement.parentElement.dataset.route;
+    description.innerHTML = this.parentElement.parentElement.dataset.descr;
+  }
 
-    function showProject(e) {
-        imgWrapper.classList.remove("dn");
-        imgWrapper.classList.add("db");
-        imagePrew.classList.add("tr03");
-        imagePrew.classList.add("op1");
-        imagePrew.src = this.parentElement.parentElement.dataset.route;
-    }
-
-    function hideProject() {
-        imagePrew.classList.remove("op1");
-    }
-
+  function hideProject() {
+    imagePrew.classList.remove("op1");
+  }
 }
 
 showProjects();
-
-
-
